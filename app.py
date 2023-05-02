74aed06fb0bde033c7e7cacd17fefbcf7d97e350
lucyscript <wathne.sveinung@gmail.com>
2023-05-02 22:51:40 +0200

    legger til index.html og routen til den.

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template, url_for, redirect
+
+app = Flask(__name__, template_folder='HTML/templates')
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
