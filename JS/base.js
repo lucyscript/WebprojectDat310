eb9f8401d6cda9c6d2795fda78d196d3b3093be3
DanyGoT <danygonzteig@gmail.com>
2023-05-05 13:06:50 +0200

    Start på sidemeny

---
+++ JS/base.js
@@ -0,0 +1,10 @@
+
+function side_menu_open() {
+    document.getElementById("side-menu").style.width = "40%";
+    document.getElementById("side-menu").style.display = "block";
+}
+
+function side_menu_close() {
+    document.getElementById("side-menu").style.width = "0";
+    document.getElementById("side-menu").style.display = "none";
+}
