1fa15a916e8b27d692192812b24cb1f57c395ed6
DanyGoT <danygonzteig@gmail.com>
2023-05-05 13:22:17 +0200

    side menu go brr

--- static/JS/base.js
+++ static/JS/base.js
@@ -1,10 +1,10 @@
 
 function side_menu_open() {
-    document.getElementById("side-menu").style.width = "40%";
-    document.getElementById("side-menu").style.display = "block";
+    document.getElementById("side_menu").style.width = "45%";
+    document.getElementById("side_menu").style.display = "block";
 }
 
 function side_menu_close() {
-    document.getElementById("side-menu").style.width = "0";
-    document.getElementById("side-menu").style.display = "none";
+    document.getElementById("side_menu").style.width = "0";
+    document.getElementById("side_menu").style.display = "none";
 }
