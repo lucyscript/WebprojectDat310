42c01986342dca3f5a6baa33b54c4903d44c4846
lucyscript <wathne.sveinung@gmail.com>
2023-05-25 11:45:24 +0200

    darkmode toggle in base

--- static/JS/base.js
+++ static/JS/base.js
@@ -71,3 +71,30 @@ search.addEventListener("input", function(event){
     xhttp.open("GET", "/search/" + event.target.value, true);
     xhttp.send();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggle = document.getElementById("toggle");
+    const root = document.documentElement;
+
+    function applyColorScheme() {
+        const isDarkMode = toggle.checked;
+
+        if (isDarkMode) {
+            root.classList.add("dark-theme");
+        } else {
+            root.classList.remove("dark-theme");
+        }
+
+        localStorage.setItem("colorScheme", isDarkMode ? "dark" : "light");
+    }
+
+    toggle.addEventListener("change", applyColorScheme);
+    
+
+    const savedColorScheme = localStorage.getItem("colorScheme");
+    if (savedColorScheme === "dark") {
+        toggle.checked = true;
+    }
+
+    applyColorScheme();
+});
