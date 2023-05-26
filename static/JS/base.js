f9b636ec79a432ab349368630e30f30cd156b512
DanyGoT <danygonzteig@gmail.com>
2023-05-26 11:19:16 +0200

    Pray to god mashalla

--- static/JS/base.js
+++ static/JS/base.js
@@ -1,36 +1,3 @@
-
-function side_menu_open() {
-    document.getElementById("side_menu").style.display = "block";
-    document.getElementById("side_menu").style.width = "45%";
-}
-
-function side_menu_close() {
-    document.getElementById("side_menu").style.display = "none";
-    document.getElementById("side_menu").style.width = "0";
-}
-
-
-function image_slide_change(n, image_index) {
-    var i;
-    var images = document.getElementsByClassName("image-slide");
-    var shown_number = document.getElementById("image-number");
-    image_index += n;
-
-    if (image_index > images.length) { image_index = 1 }
-    if (image_index < 1) { image_index = images.length }
-
-    for (i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
-    }
-
-    images[image_index - 1].style.display = "block";
-    shown_number.innerHTML = image_index + "/" + images.length;
-    document.getElementById("back-button").onclick = function () { image_slide_change(-1, image_index) };
-    document.getElementById("forward-button").onclick = function () { image_slide_change(1, image_index) };
-}
-
-
-
 // SEARCH RECOMEND
 $('#search').on('keydown', function(event) {
     if (event.keyCode === 13) {
