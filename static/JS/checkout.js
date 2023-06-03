990c265283dbb430e8bc2ec602f7bfade69d9255
lucyscript <wathne.sveinung@gmail.com>
2023-06-03 09:40:04 +0200

    successmsg on purchase

---
+++ static/JS/checkout.js
@@ -0,0 +1,14 @@
+function handleSubmit(event) {
+    console.log('TF')
+    event.preventDefault();
+
+    let button = document.getElementById('purchaseBtn');
+    let successMsg = document.getElementById('successMsg');
+
+    button.disabled = true;
+    successMsg.style.display = '';
+
+    setTimeout(function() {
+      event.target.submit();
+    }, 2000);
+  }
